1a39a326e8be480eef6c946d4636b55946ee85fb
Herr Zatacke <zatacke@gmail.com>
2020-06-12 04:02:59 +0200

    variable rename

--- websocket-gbp-emulator/blink.ino
+++ websocket-gbp-emulator/blink.ino
@@ -1,27 +1,26 @@
 bool cycle = false;
-unsigned long nextMillis = 0;
+unsigned long blink_nextMillis = 0;
 
 void wifi_blink_setup() {
   #ifdef LED_BLINK_PIN
   pinMode(LED_BLINK_PIN, OUTPUT);
   #endif
-  
-  nextMillis = millis() + WIFI_BLINK_DELAY;
+
+  blink_nextMillis = millis() + WIFI_BLINK_DELAY;
 }
 
 void wifi_blink_loop() {
   unsigned long currentMillis = millis();
 
-  if (currentMillis >= nextMillis) {
+  if (currentMillis >= blink_nextMillis) {
     if (cycle == false) {
       cycle = true;
-      nextMillis = currentMillis + 50;
-      webSocket.broadcastTXT("# gnip");
+      blink_nextMillis = currentMillis + 50;
     } else {
       cycle = false;
-      nextMillis = currentMillis + WIFI_BLINK_DELAY;
+      blink_nextMillis = currentMillis + WIFI_BLINK_DELAY;
     }
-    
+
     #ifdef LED_BLINK_PIN
     digitalWrite(LED_BLINK_PIN, cycle ? LOW : HIGH);
     #endif
