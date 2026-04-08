7e0938b70eabd4ca6061e135aca07dc30dcdbda9
Andreas Hahn <zatacke@gmail.com>
2026-04-08 02:12:03 +0200

    Basic empty vite frontend setup

---
+++ frontend/src/consts.ts
@@ -0,0 +1,6 @@
+// Api URLs
+export const URL_CLEAR_DUMPS = '/dumps/clear';
+export const URL_LIST_DUMPS = '/dumps/list';
+export const URL_GET_WIFICONFIG = '/wificonfig/get';
+export const URL_SET_WIFICONFIG = '/wificonfig/set';
+export const URL_ENV_INFO = '/env.json';
---
+++ frontend/src/index.ts
@@ -0,0 +1,17 @@
+import 'reset-css/reset.css';
+import '../styles/style.scss';
+import {$fetch} from "ofetch";
+import {URL_ENV_INFO, URL_LIST_DUMPS} from "./consts.ts";
+
+(async () => {
+  const footer = document.querySelector('.footer') as HTMLElement;
+  const main = document.querySelector('.main') as HTMLElement;
+
+
+  const info = await $fetch(URL_ENV_INFO);
+  footer.innerHTML = `<pre>${JSON.stringify(info, null, 2)}</pre>`;
+
+
+  const dumpsList = await $fetch(URL_LIST_DUMPS);
+  main.innerHTML = `<pre>${JSON.stringify(dumpsList, null, 2)}</pre>`;
+})();
