029fb6020471c7f3e84cad5b593bd10eae3620ea
Andreas Hahn <zatacke@gmail.com>
2026-04-08 02:12:03 +0200

    Basic empty vite frontend setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,71 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import { $fetch } from 'ofetch';
+
+const targetUrl = 'http://192.168.0.5';
+
+const input = fs.readdirSync(__dirname)
+  .reduce((acc: string[], file) => {
+    const filePath = path.join(__dirname, file);
+    const stats = fs.statSync(filePath);
+
+    if (!stats.isDirectory() && path.extname(file) === '.html') {
+      return [...acc, path.resolve(filePath)];
+    }
+
+    return acc;
+  }, []);
+
+export default defineConfig({
+  build: {
+    outDir: '../fs/',
+    emptyOutDir: true,
+    rolldownOptions: {
+      input,
+    }
+  },
+  plugins: [
+    {
+      name: 'custom-fetch-proxy',
+      configureServer: async (server) => {
+        console.log(`Starting proxy to ${targetUrl}`);
+        let initialResponse: object | null = null;
+        try {
+          initialResponse = await $fetch(`${targetUrl}/env.json`, { timeout: 1000 });
+        } catch {}
+
+        if (!initialResponse) {
+          console.warn(`Device at ${targetUrl} is not reachable`);
+        } else {
+          console.error(`Device at ${targetUrl} reports`, initialResponse);
+        }
+
+        server.middlewares.use(async (req, res, next) => {
+          if (
+            req.url?.startsWith('/dumps/') ||
+            req.url?.startsWith('/wificonfig/') ||
+            req.url?.startsWith('/env.json')
+          ) {
+            try {
+              const url = `${targetUrl}${req.url}`;
+              const targetResponse = await $fetch.raw(url, { method: 'GET', responseType: 'arrayBuffer', ignoreResponseError: true });
+              res.statusCode = targetResponse.status;
+              targetResponse.headers.forEach((value, name) => res.setHeader(name, value));
+              res.end(new Uint8Array(targetResponse._data || []));
+            } catch (error) {
+              console.error('Fetch proxy error:', error);
+              res.statusCode = 500;
+              res.end('Internal Server Error');
+            }
+          } else {
+            next();
+          }
+        });
+      },
+    },
+  ],
+  server: {
+    port: 3000,
+  },
+});
