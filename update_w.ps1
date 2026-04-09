c6a92bdcd05a78951f46b055229ae9b96ede84e8
Andreas Hahn <zatacke@gmail.com>
2026-04-09 16:45:00 +0200

    Version 0.3.7

--- update_w.ps1
+++ update_w.ps1
@@ -4,6 +4,6 @@ if (Test-Path ./wifi-gbp-emulator/data/w) {
 if (Test-Path ./wifi-gbp-emulator/data/pathmap.txt) {
   Remove-Item ./wifi-gbp-emulator/data/pathmap.txt -Recurse -Force -Confirm:$false
 }
-Invoke-WebRequest -Uri https://github.com/HerrZatacke/gb-printer-web/releases/latest/download/data_w.zip -OutFile ./data_w.zip
+Invoke-WebRequest -Uri https://github.com/HerrZatacke/wifi-gbp-emulator/releases/latest/download/data_w.zip -OutFile ./data_w.zip
 Expand-Archive -Path ./data_w.zip -DestinationPath ./wifi-gbp-emulator/data
 Remove-Item ./data_w.zip
--- update_w.sh
+++ update_w.sh
@@ -2,6 +2,6 @@
 
 rm -rf ./wifi-gbp-emulator/data/w
 rm -rf ./wifi-gbp-emulator/data/pathmap.txt
-curl -L https://github.com/HerrZatacke/gb-printer-web/releases/latest/download/data_w.zip --output ./data_w.zip
+curl -L https://github.com/HerrZatacke/wifi-gbp-emulator/releases/latest/download/data_w.zip --output ./data_w.zip
 unzip ./data_w.zip -d ./wifi-gbp-emulator/data
 rm ./data_w.zip
